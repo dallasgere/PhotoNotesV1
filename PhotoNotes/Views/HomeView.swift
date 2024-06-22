ce095b4032fd22a11bc8e4818eb3adb7a5da75c8
dallas <dallasmgere@gmail.com>
2024-06-21 19:51:09 -0500

    adding the initial homepage

--- PhotoNotes/Views/HomeView.swift
+++ PhotoNotes/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     @ObservedObject private var homeViewModel: HomeViewModel = HomeViewModel()
+    @State private var isHomeSheetActive = false
     
     var body: some View {
         NavigationStack {
@@ -16,6 +17,16 @@ struct HomeView: View {
                 ForEach(homeViewModel.homeModel) { photo in
                     
                 }
+            }
+            .navigationTitle("Photo Notes")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing, content: {
+                    Button(action: {
+                        self.isHomeSheetActive = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                })
             }
         }
     }
